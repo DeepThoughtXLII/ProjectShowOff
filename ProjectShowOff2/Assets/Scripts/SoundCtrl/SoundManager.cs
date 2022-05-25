1dd5ce80c0a92a1d8ffcf39ebe7f80c4cffb8ee2
0MADXX <marcosrane@gmail.com>
2022-05-25 12:25:15 +0200

    Sound controller built in // MAIN MENU UI // CHARACTER CONTROLLER 2D //

---
+++ ProjectShowOff2/Assets/Scripts/SoundCtrl/SoundManager.cs
@@ -0,0 +1,57 @@
+using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+
+public class SoundManager : MonoBehaviour
+{
+    // store sounds in array
+    public Sound[] sounds;
+    public AudioMixerGroup mixer;
+
+    public static SoundManager instance;
+
+    void Awake()
+    {
+
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+        // for each sound set vars
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.outputAudioMixerGroup = mixer;
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+
+
+        }
+    }
+
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Play();
+    }
+
+    public void Stop(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Stop();
+    }
+
+
+
+}
